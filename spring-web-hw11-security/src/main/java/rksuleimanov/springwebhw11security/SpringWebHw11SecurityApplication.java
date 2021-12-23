828de8338afaf4bfbd67f5639eca19b7c8631911
crangoren <crangoren@gmail.com>
2021-12-23 14:14:22 +0300

    add project to doing homework

---
+++ spring-web-hw11-security/src/main/java/rksuleimanov/springwebhw11security/SpringWebHw11SecurityApplication.java
@@ -0,0 +1,13 @@
+package rksuleimanov.springwebhw11security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebHw11SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebHw11SecurityApplication.class, args);
+	}
+
+}
---
+++ spring-web-hw11-security/src/test/java/rksuleimanov/springwebhw11security/SpringWebHw11SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package rksuleimanov.springwebhw11security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebHw11SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
